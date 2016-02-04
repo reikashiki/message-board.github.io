f95e1d5225b3eb9aef3a9ff9c052abdfe1ffe887
reikashiki <audrey.fa.audrey@gmail.com>
2016-02-04 12:43:16 +0000

    create message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,5 @@ class Message < ActiveRecord::Base
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
   #年齢０歳以上
-  validates :age , numericality: { greater_than: 0 , less_than:120 } , presence: true
+  validates :age , numericality: { greater_than_or_equal_to: 0 , less_than:120 } , presence: true
 end
